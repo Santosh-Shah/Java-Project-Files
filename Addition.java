5f9010ff8b6524811a3030b4f25bd0863515819a
Santosh Shah <santoshofficial679@gmail.com>
2023-02-18 09:52:30 +0545

    add multiply file

--- Addition.java
+++ Addition.java
@@ -1,6 +1,6 @@
 public class Addition {
     public static void main(String[] args) {
-        int a = 50;
+        int a = 5;
         int b = 55;
         int add = a + b;
         System.out.println(add);
